763209172e16f549bad680bb0d2e2cb531370842
Young-Zen <2453883990@qq.com>
2023-09-26 19:25:13 +0800

    Optimize

--- src/main/java/com/sz/springbootsample/demo/dto/ResponseResultDTO.java
+++ src/main/java/com/sz/springbootsample/demo/dto/ResponseResultDTO.java
@@ -15,48 +15,41 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @ApiModel(description = "响应返回结果")
-public class ResponseResultDTO {
+public class ResponseResultDTO<T> {
     @ApiModelProperty("响应码")
     private String code;
     @ApiModelProperty("响应消息")
     private String msg;
     @ApiModelProperty("响应数据")
-    private Object data;
+    private T data;
 
-    public <T> T getData(Class<T> clazz) {
-        if (clazz.isInstance(data)) {
-            return clazz.cast(data);
-        }
-        return null;
-    }
-
-    public static ResponseResultDTO ok() {
+    public static <T> ResponseResultDTO<T> ok() {
         ResponseResultDTO responseResultDTO = new ResponseResultDTO();
         responseResultDTO.setCode(ResponseCodeEnum.OK.getCode());
         responseResultDTO.setMsg(ResponseCodeEnum.OK.getMsg());
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO ok(String msg) {
+    public static <T> ResponseResultDTO<T> ok(String msg) {
         ResponseResultDTO responseResultDTO = ok();
         responseResultDTO.setMsg(msg);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO ok(Object data) {
+    public static <T> ResponseResultDTO<T> ok(T data) {
         ResponseResultDTO responseResultDTO = ok();
         responseResultDTO.setData(data);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO ok(String msg, Object data) {
+    public static <T> ResponseResultDTO<T> ok(String msg, T data) {
         ResponseResultDTO responseResultDTO = ok();
         responseResultDTO.setMsg(msg);
         responseResultDTO.setData(data);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO ok(String code, String msg, Object data) {
+    public static <T> ResponseResultDTO<T> ok(String code, String msg, T data) {
         ResponseResultDTO responseResultDTO = ok();
         responseResultDTO.setCode(code);
         responseResultDTO.setMsg(msg);
@@ -64,41 +57,41 @@ public class ResponseResultDTO {
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO ok(String code, String msg) {
+    public static <T> ResponseResultDTO<T> ok(String code, String msg) {
         ResponseResultDTO responseResultDTO = ok();
         responseResultDTO.setCode(code);
         responseResultDTO.setMsg(msg);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO fail() {
+    public static <T> ResponseResultDTO<T> fail() {
         ResponseResultDTO responseResultDTO = new ResponseResultDTO();
         responseResultDTO.setCode(ResponseCodeEnum.FAIL.getCode());
         responseResultDTO.setMsg(ResponseCodeEnum.FAIL.getMsg());
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO fail(ResponseCodeEnum responseCodeEnum) {
+    public static <T> ResponseResultDTO<T> fail(ResponseCodeEnum responseCodeEnum) {
         ResponseResultDTO responseResultDTO = new ResponseResultDTO();
         responseResultDTO.setCode(responseCodeEnum.getCode());
         responseResultDTO.setMsg(responseCodeEnum.getMsg());
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO fail(String msg) {
+    public static <T> ResponseResultDTO<T> fail(String msg) {
         ResponseResultDTO responseResultDTO = fail();
         responseResultDTO.setMsg(msg);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO fail(String errCode, String msg) {
+    public static <T> ResponseResultDTO<T> fail(String errCode, String msg) {
         ResponseResultDTO responseResultDTO = new ResponseResultDTO();
         responseResultDTO.setCode(errCode);
         responseResultDTO.setMsg(msg);
         return responseResultDTO;
     }
 
-    public static ResponseResultDTO fail(String errCode, String msg, Object data) {
+    public static <T> ResponseResultDTO<T> fail(String errCode, String msg, T data) {
         ResponseResultDTO responseResultDTO = new ResponseResultDTO();
         responseResultDTO.setCode(errCode);
         responseResultDTO.setMsg(msg);
