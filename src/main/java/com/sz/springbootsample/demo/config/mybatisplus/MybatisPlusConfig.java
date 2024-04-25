bae08c3974829a7edd93729e6e769d5b03193060
yanghaojia <2453883990@qq.com>
2024-04-25 14:57:15 +0800

    PaginationInnerInterceptor does not configure DbType when there are multiple data sources

--- src/main/java/com/sz/springbootsample/demo/config/mybatisplus/MybatisPlusConfig.java
+++ src/main/java/com/sz/springbootsample/demo/config/mybatisplus/MybatisPlusConfig.java
@@ -3,7 +3,6 @@ package com.sz.springbootsample.demo.config.mybatisplus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
 import com.baomidou.mybatisplus.extension.incrementer.H2KeyGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
@@ -22,7 +21,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         // 如果配置多个插件,切记分页最后添加interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         // 如果有多数据源可以不配具体类型,否则都建议配上具体的DbType
         return interceptor;
