66ac2e1dea07c39a652fd8377fb70e277c205135
yanghaojia <2453883990@qq.com>
2025-05-23 10:50:17 +0800

    Disable flyway to replace placeholders

--- scripts/sql_migrate.sh
+++ scripts/sql_migrate.sh
@@ -9,4 +9,4 @@ FLYWAY_TABLE=$5
 FLYWAY_LOCATIONS=$6
 
 # https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/7.15.0/flyway-commandline-7.15.0.tar.gz
-/usr/bin/flyway -community -url="${FLYWAY_URL}" -user="${FLYWAY_USER}" -password="${FLYWAY_PASSWORD}" -cleanDisabled=true -baselineOnMigrate=true -baselineVersion=1 -encoding=UTF-8 -outOfOrder=false -validateOnMigrate=true -schemas="${FLYWAY_SCHEMAS}" -table="${FLYWAY_TABLE}" -locations="${FLYWAY_LOCATIONS}" migrate
+/usr/bin/flyway -community -url="${FLYWAY_URL}" -user="${FLYWAY_USER}" -password="${FLYWAY_PASSWORD}" -placeholderReplacement=false -cleanDisabled=true -baselineOnMigrate=true -baselineVersion=1 -encoding=UTF-8 -outOfOrder=false -validateOnMigrate=true -schemas="${FLYWAY_SCHEMAS}" -table="${FLYWAY_TABLE}" -locations="${FLYWAY_LOCATIONS}" migrate
